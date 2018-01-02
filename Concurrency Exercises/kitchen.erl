4ab433961eb4ab0e0cb00eb1ec3dcfa8bffacd51
VetoPlayer <andrewlloys@gmail.com>
2018-01-02 14:32:45 +0100

    Message hiding and timeout

--- Concurrency Exercises/kitchen.erl
+++ Concurrency Exercises/kitchen.erl
@@ -24,3 +24,32 @@ fridge(FoodList) ->
         terminate ->
             ok
     end.
+
+%% Let's define a standard way to interact with a fridge from the shell without forcing the shell to know how
+%% the message is passed
+
+%% We define a standard wrapper function for each message corresponding to an exposed functionality:
+
+store(FridgeId, Item) -> %% Hide the protocol for message passing
+    FridgeId ! {self(),{store,Item}}, %% Send a message to the fridge
+    receive
+        {FridgeId,Msg} -> Msg %% Waits to receive a response and return it.
+    after 3000 ->
+        timeout
+    end.
+
+take(FridgeId,Item) ->
+    FridgeId ! {self(), {take, Item}},
+    receive
+        {FridgeId,Msg} -> Msg
+    after 3000 ->
+        timeout
+    end.
+
+%% We create a standard way to create the fridge too:
+
+start(FoodList) ->
+    spawn(?MODULE, fridge, [FoodList]).
+
+%% Everything about the fridge process is now handled in the kitchen.erl module
+
